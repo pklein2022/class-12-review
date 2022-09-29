eaef3520166d25b49b339d1865883b0ef3828e46
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-29 18:02:28 -0500

    warm-up-2

--- warm-up-2.js
+++ warm-up-2.js
@@ -1,11 +1,17 @@
-//Define a function called reverse with one parameter. This function will reverse the string. 
+//Define a function called reverse with one parameter. This function will reverse the string.
 //You cannot use any built in methods to do this. Use a loop.
 
 let sentence = "My name is John!";
 function reverse(str) {
-  for (let i = 0; i < str.length; i++) {
-    text += "The number is " + i + "<br>";
-  }
+  const reverseString = (str = "") => {
+    let reverse = "";
+    const { length: len } = str;
+    for (let i = len - 1; i >= 0; i--) {
+      reverse += str[i];
+    }
+    return reverse;
+  };
+  console.log(reverseString(str));
 }
 
 reverse(sentence); // "!nhoJ si eman yM"
