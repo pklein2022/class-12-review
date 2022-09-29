d5bdfb9480763df80ababf61f4e57d2864877db3
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-29 17:54:44 -0500

    Initial commit

---
+++ warm-up-1.js
@@ -0,0 +1,13 @@
+//Define a function called updateNames with one parameter. 
+//This function will replace the name John with the given parameter.
+
+
+let name = "Amy";
+function updateNames(name) {
+    
+    let sentence = "Hello, my name is John. My friend's name is also John.";
+    let result = sentence.replaceAll("John", (name));
+    console.log(result);
+}
+
+updateNames(name); // "Hello, my name is Amy. My friend's name is also Amy."
---
+++ warm-up-2.js
@@ -0,0 +1,11 @@
+//Define a function called reverse with one parameter. This function will reverse the string. 
+//You cannot use any built in methods to do this. Use a loop.
+
+let sentence = "My name is John!";
+function reverse(str) {
+  for (let i = 0; i < str.length; i++) {
+    text += "The number is " + i + "<br>";
+  }
+}
+
+reverse(sentence); // "!nhoJ si eman yM"
