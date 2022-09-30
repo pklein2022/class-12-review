2f739a06ffba32a57832665c612d7bb177af4033
pklein2022 <111322242+pklein2022@users.noreply.github.com>
2022-09-29 19:14:05 -0500

    updated folder name

--- warm-up-1.js
+++ warm-up-1.js
@@ -6,8 +6,11 @@ let name = "Amy";
 function updateNames(name) {
     
     let sentence = "Hello, my name is John. My friend's name is also John.";
-    let result = sentence.replaceAll("John", (name));
+    let result = sentence.replaceAll("John", name);
     console.log(result);
 }
 
 updateNames(name); // "Hello, my name is Amy. My friend's name is also Amy."
+
+//return.sentence.replace(/John/g, name)
+//regex expression - regexr.com
--- warm-up-2.js
+++ warm-up-2.js
@@ -1,17 +1,30 @@
 //Define a function called reverse with one parameter. This function will reverse the string.
 //You cannot use any built in methods to do this. Use a loop.
 
+// let sentence = "My name is John!";
+// function reverse(str) {
+//   const reverseString = (str = "") => {
+//     let reverse = "";
+//     const { length: len } = str;
+//     for (let i = len - 1; i >= 0; i--) {
+//       reverse += str[i];
+//     }
+//     return reverse;
+//   };
+//   console.log(reverseString(str));
+// }
+
+// reverse(sentence); // "!nhoJ si eman yM"
+
+//class answer
 let sentence = "My name is John!";
 function reverse(str) {
-  const reverseString = (str = "") => {
-    let reverse = "";
-    const { length: len } = str;
-    for (let i = len - 1; i >= 0; i--) {
-      reverse += str[i];
-    }
-    return reverse;
-  };
-  console.log(reverseString(str));
-}
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; --i) {
+    newStr += str[i];
+  }
+  return newStr;
 
-reverse(sentence); // "!nhoJ si eman yM"
+}
+const r = reverse(sentence);
+console.log(r);
